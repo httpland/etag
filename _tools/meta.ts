a883f7c3dbb9b9980d641d8be389a7e7fac99962
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-02-11 16:04:35 +0900

    chore(_tools): add npm build script

---
+++ _tools/meta.ts
@@ -0,0 +1,39 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.33.1/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {},
+  compilerOptions: {
+    lib: ["esnext", "dom"],
+  },
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "@httpland/etag",
+    version,
+    description: "HTTP ETag utilities for standard Request and Response",
+    keywords: [
+      "http",
+      "etag",
+      "hash",
+      "entity",
+      "handler",
+      "request",
+      "response",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/httpland/etag",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/httpland/etag.git",
+    },
+    bugs: {
+      url: "https://github.com/httpland/etag/issues",
+    },
+    sideEffects: false,
+    type: "module",
+    publishConfig: { access: "public" },
+  },
+  packageManager: "pnpm",
+});
